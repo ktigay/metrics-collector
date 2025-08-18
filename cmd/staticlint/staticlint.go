ca8c95c08a3c871f9aeae6ea3b9fa7ef4efd483a
Тигай Константин <konstantin.tigai@lenta.com>
2025-08-18 22:25:59 +0300

    iter19 add staticlint

---
+++ cmd/staticlint/staticlint.go
@@ -0,0 +1,161 @@
+// Package main Содержит:
+// - Стандартные статические анализаторы пакета golang.org/x/tools/go/analysis/passes;
+// - Все анализаторы класса SA пакета staticcheck.io;
+// - s1002, s1003 (simple) пакета staticcheck.io;
+// - st1001, st1005, st1006 (stylecheck) пакета staticcheck.io;
+// - qf1004, qf1006 (quickfix) пакета staticcheck.io;
+// - Анализатор [errcheck](https://github.com/kisielk/errcheck/errcheck);
+// - Анализатор [whitespace](https://github.com/ultraware/whitespace);
+// - Анализатор osexit, проверяет использование os.Exit в пакете main, функции main.
+package main
+
+import (
+	"github.com/kisielk/errcheck/errcheck"
+	"github.com/ultraware/whitespace"
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/multichecker"
+	"golang.org/x/tools/go/analysis/passes/appends"
+	"golang.org/x/tools/go/analysis/passes/asmdecl"
+	"golang.org/x/tools/go/analysis/passes/assign"
+	"golang.org/x/tools/go/analysis/passes/atomic"
+	"golang.org/x/tools/go/analysis/passes/atomicalign"
+	"golang.org/x/tools/go/analysis/passes/bools"
+	"golang.org/x/tools/go/analysis/passes/buildssa"
+	"golang.org/x/tools/go/analysis/passes/buildtag"
+	"golang.org/x/tools/go/analysis/passes/cgocall"
+	"golang.org/x/tools/go/analysis/passes/composite"
+	"golang.org/x/tools/go/analysis/passes/copylock"
+	"golang.org/x/tools/go/analysis/passes/ctrlflow"
+	"golang.org/x/tools/go/analysis/passes/deepequalerrors"
+	"golang.org/x/tools/go/analysis/passes/defers"
+	"golang.org/x/tools/go/analysis/passes/directive"
+	"golang.org/x/tools/go/analysis/passes/errorsas"
+	"golang.org/x/tools/go/analysis/passes/fieldalignment"
+	"golang.org/x/tools/go/analysis/passes/findcall"
+	"golang.org/x/tools/go/analysis/passes/framepointer"
+	"golang.org/x/tools/go/analysis/passes/gofix"
+	"golang.org/x/tools/go/analysis/passes/hostport"
+	"golang.org/x/tools/go/analysis/passes/httpmux"
+	"golang.org/x/tools/go/analysis/passes/httpresponse"
+	"golang.org/x/tools/go/analysis/passes/ifaceassert"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/analysis/passes/loopclosure"
+	"golang.org/x/tools/go/analysis/passes/lostcancel"
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
+	"golang.org/x/tools/go/analysis/passes/nilness"
+	"golang.org/x/tools/go/analysis/passes/pkgfact"
+	"golang.org/x/tools/go/analysis/passes/printf"
+	"golang.org/x/tools/go/analysis/passes/reflectvaluecompare"
+	"golang.org/x/tools/go/analysis/passes/shadow"
+	"golang.org/x/tools/go/analysis/passes/shift"
+	"golang.org/x/tools/go/analysis/passes/sigchanyzer"
+	"golang.org/x/tools/go/analysis/passes/slog"
+	"golang.org/x/tools/go/analysis/passes/sortslice"
+	"golang.org/x/tools/go/analysis/passes/stdmethods"
+	"golang.org/x/tools/go/analysis/passes/stdversion"
+	"golang.org/x/tools/go/analysis/passes/stringintconv"
+	"golang.org/x/tools/go/analysis/passes/structtag"
+	"golang.org/x/tools/go/analysis/passes/testinggoroutine"
+	"golang.org/x/tools/go/analysis/passes/tests"
+	"golang.org/x/tools/go/analysis/passes/timeformat"
+	"golang.org/x/tools/go/analysis/passes/unmarshal"
+	"golang.org/x/tools/go/analysis/passes/unreachable"
+	"golang.org/x/tools/go/analysis/passes/unsafeptr"
+	"golang.org/x/tools/go/analysis/passes/unusedresult"
+	"golang.org/x/tools/go/analysis/passes/unusedwrite"
+	"golang.org/x/tools/go/analysis/passes/usesgenerics"
+	"golang.org/x/tools/go/analysis/passes/waitgroup"
+	"honnef.co/go/tools/quickfix/qf1004"
+	"honnef.co/go/tools/quickfix/qf1006"
+	"honnef.co/go/tools/simple/s1002"
+	"honnef.co/go/tools/simple/s1003"
+	"honnef.co/go/tools/staticcheck"
+	"honnef.co/go/tools/stylecheck/st1001"
+	"honnef.co/go/tools/stylecheck/st1005"
+	"honnef.co/go/tools/stylecheck/st1006"
+
+	"github.com/ktigay/metrics-collector/internal/staticlint/osexit"
+)
+
+// Анализаторы
+func main() {
+	sch := []*analysis.Analyzer{
+		// анализаторы из пакета passes.
+		appends.Analyzer,
+		asmdecl.Analyzer,
+		assign.Analyzer,
+		atomic.Analyzer,
+		atomicalign.Analyzer,
+		bools.Analyzer,
+		buildssa.Analyzer,
+		buildtag.Analyzer,
+		cgocall.Analyzer,
+		composite.Analyzer,
+		copylock.Analyzer,
+		ctrlflow.Analyzer,
+		deepequalerrors.Analyzer,
+		defers.Analyzer,
+		directive.Analyzer,
+		errorsas.Analyzer,
+		fieldalignment.Analyzer,
+		findcall.Analyzer,
+		framepointer.Analyzer,
+		gofix.Analyzer,
+		hostport.Analyzer,
+		httpmux.Analyzer,
+		httpresponse.Analyzer,
+		ifaceassert.Analyzer,
+		inspect.Analyzer,
+		loopclosure.Analyzer,
+		lostcancel.Analyzer,
+		nilfunc.Analyzer,
+		nilness.Analyzer,
+		pkgfact.Analyzer,
+		printf.Analyzer,
+		reflectvaluecompare.Analyzer,
+		shadow.Analyzer,
+		shift.Analyzer,
+		sigchanyzer.Analyzer,
+		slog.Analyzer,
+		sortslice.Analyzer,
+		stdmethods.Analyzer,
+		stdversion.Analyzer,
+		stringintconv.Analyzer,
+		structtag.Analyzer,
+		testinggoroutine.Analyzer,
+		tests.Analyzer,
+		timeformat.Analyzer,
+		unmarshal.Analyzer,
+		unreachable.Analyzer,
+		unsafeptr.Analyzer,
+		unusedresult.Analyzer,
+		unusedwrite.Analyzer,
+		usesgenerics.Analyzer,
+		waitgroup.Analyzer,
+		// simple.
+		s1002.Analyzer,
+		s1003.Analyzer,
+		// stylecheck.
+		st1001.Analyzer,
+		st1005.Analyzer,
+		st1006.Analyzer,
+		// quickfix.
+		qf1004.Analyzer,
+		qf1006.Analyzer,
+		// errcheck.
+		errcheck.Analyzer,
+		// whitespace.
+		whitespace.NewAnalyzer(nil),
+		// osexit
+		osexit.Analyzer,
+	}
+
+	// анализаторы staticcheck SA*
+	for _, v := range staticcheck.Analyzers {
+		sch = append(sch, v.Analyzer)
+	}
+
+	multichecker.Main(
+		sch...,
+	)
+}
---
+++ internal/staticlint/osexit/analyzer_test.go
@@ -0,0 +1,11 @@
+package osexit
+
+import (
+	"testing"
+
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+func Test_run(t *testing.T) {
+	analysistest.Run(t, analysistest.TestData(), Analyzer, "./...")
+}
